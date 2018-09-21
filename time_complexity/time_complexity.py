15118851ba1f1a6784ee66afc244fe4160d9ea39
Victor-Johannes Holl <victor.johannes.holl@gmail.com>
2018-09-21 15:51:45 +0200

    Plots time complexity

--- time_complexity/time_complexity.py
+++ time_complexity/time_complexity.py
@@ -1,20 +1,33 @@
 import random
 import time
+import matplotlib.pyplot as plt
 
-for n in (3:50):
-    state = list(range( 1, n ))
-    random.shuffle( state )
-    print( state )
+results = []
+decksizes = [10, 100, 1000, 10000]
+repetition_limit = 100
 
-    start = time.time()
-    j = 1
-    for j in range(j, len(state)):
-        key = state[j]
-        i = j - 1
-        while i >= 0 and state[i] > key:
-          state[i + 1] = state[i]
-          i = i - 1
-        state[i + 1] = key
-    time_consumption = time.time() - start
-    print (state)
-    print("operation took " + " {0:.9f}".format(time_consumption) + " seconds")
+for n in range(0, len(decksizes)):
+    sum = 0
+    for repetition in range( 1, repetition_limit ):
+        state = list(range( 1, decksizes[n] ))
+        random.shuffle( state )
+
+        start = time.time()
+        j = 1
+        for j in range(j, len(state)):
+            key = state[j]
+            i = j - 1
+            while i >= 0 and state[i] > key:
+                state[i + 1] = state[i]
+                i = i - 1
+            state[i + 1] = key
+        sum = sum + ( time.time() - start )
+        # print (state)
+        # print("operation took " + " {0:.9f}".format( time.time() - start ) + " seconds")
+    average = sum / repetition_limit
+    print(sum)
+    results.append ( average )
+    
+plt.plot( results )
+plt.ylabel('time complexity')
+plt.show()
